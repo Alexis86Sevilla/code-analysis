f0c67499b6e96ea065bb13d05e4b9f96b5886380
alexis <alexis@NUNLTS1404.NUNSYS.INT>
2025-09-18 22:22:04 +0200

    implementado formulario simple y llamada a github

---
+++ src/pages/api/github.js
@@ -0,0 +1,16 @@
+export const prerender = false;
+
+export async function POST(request) {
+    const formData = await request.request.formData();
+    const user = formData.get('user');
+    const repository = formData.get('repository');
+
+    const response = await fetch("https://api.github.com/repos/" + user + "/" + repository + "", {
+        method: "GET",
+
+    });
+
+    const data = await response.json();
+    console.warn(data)
+    return new Response(JSON.stringify(data));
+}
